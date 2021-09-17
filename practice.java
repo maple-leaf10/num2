8772dae775d7ff74cdf99b6bb22e6b0df11c09b8
maple-leaf10 <75773766+maple-leaf10@users.noreply.github.com>
2021-09-17 19:38:13 +0800

    Create practice.java

---
+++ practice.java
@@ -0,0 +1,10 @@
+import java.io.FileNotFoundException;
+
+public class practice {
+    public static void main (String[] args) {
+        String line = "printf (\"int\")";
+        System.out.println(line);
+        String l = line.replace("\"","a");
+        System.out.println(l);
+    }
+}
